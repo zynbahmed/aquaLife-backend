a804b6e064d0a3ea27cbf3d77d79db94d0aa3e6c
zynbahmed <201901241@student.polytechnic.bh>
2024-02-29 15:38:26 +0300

    server.js updated

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const logger = require('morgan')
 const cors = require('cors')
 
 const AuthRouter = require('./routes/AuthRouter')
-const PostRouter = require('./routes/PostRouter')
+const ActivityRouter = require('./routes/ActivityRouter')
+const ReviewRouter = require('./routes/ReviewRouter')
+const BookingRouter = require('./routes/BookingRouter')
 
 const PORT = process.env.PORT || 3001
 
@@ -17,7 +19,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/auth', AuthRouter)
-app.use('/posts', PostRouter)
+app.use('/activities', ActivityRouter)
+app.use('/reviews', ReviewRouter)
+app.use('/bookings', BookingRouter)
 
 app.use('/', (req, res) => {
   res.send(`Connected!`)
