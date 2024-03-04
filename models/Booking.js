7b571dd09439d4a80a5f52768b04de42c10833a6
zynbahmed <201901241@student.polytechnic.bh>
2024-03-04 15:36:03 +0300

    removed comma in bookingmodel

--- models/Booking.js
+++ models/Booking.js
@@ -5,7 +5,7 @@ const bookingSchema = new Schema(
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
     activities: [{ type: Schema.Types.ObjectId, ref: "Activity" }],
     quantity: { type: Number, default: 1 },
-    totalPrice: { type: Number },
+    totalPrice: { type: Number }
   },
   { timestamps: true }
 )
