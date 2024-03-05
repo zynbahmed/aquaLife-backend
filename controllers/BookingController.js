e4fe7423caf77dc9e47552d3e862336fcd087b08
Nayef Alzayani <nzayani57@outlook.com>
2024-03-05 11:24:32 +0300

    cart n bookings

--- controllers/BookingController.js
+++ controllers/BookingController.js
@@ -11,8 +11,25 @@ const GetBooking = async (req, res) => {
 
 const CreateBooking = async (req, res) => {
   try {
-    const booking = await Booking.create({ ...req.body })
-    res.send(booking)
+    let booking = []
+    let totalPrice = 0
+    for (let i = 0; i < req.body.cart.length; i++) {
+      booking.push(req.body.cart[i]._id)
+      totalPrice =
+        totalPrice + req.body.cart[i].userQty * req.body.cart[i].price
+    }
+    const activityBooking = await Booking.create({
+      activities: booking,
+      totalPrice: totalPrice,
+      user: req.body,
+      user_id
+    })
+    await user.updateOne(
+      { _id: req.body.user._id },
+      { $push: { bookings: activityBooking._id } }
+    )
+    // const booking = await Booking.create({ ...req.body })
+    // res.send(booking)
   } catch (error) {
     throw error
   }
