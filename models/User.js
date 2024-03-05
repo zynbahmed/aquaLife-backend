d228ab072364418188b6984245b6303d2801e513
zynbahmed <201901241@student.polytechnic.bh>
2024-03-05 20:00:38 +0300

    added the profilePic in the user model

--- models/User.js
+++ models/User.js
@@ -1,12 +1,17 @@
-const { Schema } = require('mongoose')
+const { Schema } = require("mongoose")
 
 const userSchema = new Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true },
     passwordDigest: { type: String, required: true },
-    userType: {type:String , enum:["admin","user"] ,default:"user"},
-    bookings: [{ type: Schema.Types.ObjectId, ref: 'Booking' }]
+    userType: { type: String, enum: ["admin", "user"], default: "user" },
+    bookings: [{ type: Schema.Types.ObjectId, ref: "Booking" }],
+    profilePic: {
+      type: String,
+      default:
+        "https://tr.rbxcdn.com/38c6edcb50633730ff4cf39ac8859840/420/420/Hat/Png",
+    },
   },
   { timestamps: true }
 )
